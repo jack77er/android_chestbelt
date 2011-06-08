fd0538b81b4c71657541c41959c820e87549b57c
jack77 <jack77@gmx.net>
2011-06-08 12:17:44 +0200

    discovery läuft liste zur deviceauswahl erstellt

--- src/com/cestbelt/test/cest_activity.java
+++ src/com/cestbelt/test/cest_activity.java
@@ -1,6 +1,11 @@
 package com.cestbelt.test;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,29 +14,60 @@ import android.widget.Toast;
 import android.bluetooth.*;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 
 public class cest_activity extends Activity {
+	
+	private final Context mainContext = this;
 	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
 	
 	public final static int REQUEST_ENABLE_BT = 0;
-	private boolean btWasEnabled = false;
+	private boolean btWasEnabled = true;
+	private boolean btisEnabled = false;
 	
-	// UI View elements
+	static final int DIALOG_BT_SCANNING = 1;
 	
+	
+	
+	private ArrayList<BluetoothDevice> remoteDevices;
+	private BluetoothDevice choosenDevice;
+
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bcastReceiver, filter); // Don't forget to unregister during onDestroy
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bcastReceiver, filter); // Don't forget to unregister during onDestroy
+        
+        
+        remoteDevices = new ArrayList<BluetoothDevice>();
+        
         setContentView(R.layout.main);
-        Button btnRescan = (Button)findViewById(R.id.btnRescan);
-        btnRescan.setOnClickListener(new OnClickListener() {
+        ((Button)findViewById(R.id.btnRescan)).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), "button clicked", Toast.LENGTH_SHORT).show();	
+				if(!adapter.isEnabled()) {
+		    		// bluetooth disabled
+		    		Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    		startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);    	
+		    	} else {		    		
+		    		// interrupt discovery if already started
+		    		adapter.cancelDiscovery();
+		    		if(adapter.startDiscovery()) {
+		    			//Toast.makeText(getApplicationContext(), "scan initiated", Toast.LENGTH_SHORT).show();
+		    			showDialog(DIALOG_BT_SCANNING);
+		    		} else {
+		    			Toast.makeText(getApplicationContext(), "scan failed", Toast.LENGTH_SHORT).show();
+		    		}
+		    	}
+				
 			}
           });
 
@@ -46,27 +82,33 @@ public class cest_activity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        
-    }
+        if(!btWasEnabled) {
+        	// quick and dirty
+        	adapter.disable();
+        }
+	}
+
+    /**
+     * Method for creating dialogs
+     */
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		if (id == DIALOG_BT_SCANNING) {
+			ProgressDialog loadingDialog = new ProgressDialog(this);
+			loadingDialog.setMessage("searching...");
+			loadingDialog.setIndeterminate(true);
+			loadingDialog.setCancelable(true);
+			return loadingDialog;
+		}
+		return super.onCreateDialog(id);
+	}
+
     
     private void scanForCestbelt() {
 
     }
     
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_ENABLE_BT) {
-			System.out.println(resultCode);
-			if (resultCode == RESULT_CANCELED) {
-				Toast.makeText(getApplicationContext(), "bt not available", Toast.LENGTH_SHORT).show();		
-				scanForCestbelt();
-			} else {
-				Toast.makeText(getApplicationContext(), "bt enabled AR", Toast.LENGTH_SHORT).show();
-			}
-		}				
-	}
-	
-	private final BroadcastReceiver bcastReceiver = new BroadcastReceiver() {
+    private final BroadcastReceiver bcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -76,14 +118,63 @@ public class cest_activity extends Activity {
                 switch (state) {
                 case BluetoothAdapter.STATE_ON:
                 	Toast.makeText(getApplicationContext(), "bt enabled", Toast.LENGTH_SHORT).show();
+                	btWasEnabled = false;
+                	btisEnabled = true;
                     //onBluetoothEnable();
                     break;
                 case BluetoothAdapter.STATE_TURNING_OFF:
                 	Toast.makeText(getApplicationContext(), "bt disabled", Toast.LENGTH_SHORT).show();
+                	btisEnabled = false;
                     //onBluetoothDisable();
                     break;
                 }
+            } else if(action.equals(BluetoothDevice.ACTION_FOUND)) {
+            	if(!remoteDevices.contains((BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))) {
+            		remoteDevices.add((BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
+            	}
+            } else if(action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+            	//Toast.makeText(getApplicationContext(), "scan finished", Toast.LENGTH_SHORT).show();
+            	
+            	AlertDialog.Builder builder = new AlertDialog.Builder(mainContext);
+            	builder.setTitle("Choose a Device");
+            	
+            	final CharSequence[] items = new CharSequence[remoteDevices.size()];
+            	for(int i = 0 ; i < remoteDevices.size() ; i++) {
+            		items[i] = remoteDevices.get(i).getName();
+            	}
+            	
+            	builder.setItems(items, new DialogInterface.OnClickListener() {
+            	    public void onClick(DialogInterface dialog, int item) {
+            	        choosenDevice = remoteDevices.get(item);
+            	    }
+            	});
+            	
+            	dismissDialog(DIALOG_BT_SCANNING);
+            	AlertDialog alert = builder.create();
+            	alert.show();
+            	
             } 
         }
     };
+    
+    
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			System.out.println(resultCode);
+			if (resultCode == RESULT_CANCELED) {
+				Toast.makeText(getApplicationContext(), "bt not available", Toast.LENGTH_SHORT).show();	
+				btisEnabled = false;
+			} else {
+				Toast.makeText(getApplicationContext(), "bt enabled AR", Toast.LENGTH_SHORT).show();
+				btWasEnabled = false;
+				btisEnabled = true;
+				scanForCestbelt();
+			}
+		}				
+	}
+	
+	
+
+    
 }
