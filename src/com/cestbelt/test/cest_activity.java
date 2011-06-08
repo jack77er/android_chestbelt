58e907d22ae12a1d84719ea101c299d81f70c3f7
jack77 <jack77@gmx.net>
2011-06-08 09:45:59 +0200

    First try

--- src/com/cestbelt/test/cest_activity.java
+++ src/com/cestbelt/test/cest_activity.java
@@ -2,12 +2,73 @@ package com.cestbelt.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.bluetooth.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
 public class cest_activity extends Activity {
+	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+	
+	public final static int REQUEST_ENABLE_BT = 0;
+	private boolean btWasEnabled = false;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    	if(!adapter.isEnabled()) {
+    		// bluetooth disabled
+    		Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+    		startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);    	
+    	}
+    	
     }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        
+    }
+    
+    private void scanForCestbelt() {
+
+    }
+    
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			System.out.println(resultCode);
+			if (resultCode == RESULT_CANCELED) {
+				Toast.makeText(getApplicationContext(), "bt not available", Toast.LENGTH_SHORT).show();		
+				scanForCestbelt();
+			} else {
+				Toast.makeText(getApplicationContext(), "bt enabled AR", Toast.LENGTH_SHORT).show();
+			}
+		}				
+	}
+	
+	private final BroadcastReceiver bcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                switch (state) {
+                case BluetoothAdapter.STATE_ON:
+                	Toast.makeText(getApplicationContext(), "bt enabled", Toast.LENGTH_SHORT).show();
+                    //onBluetoothEnable();
+                    break;
+                case BluetoothAdapter.STATE_TURNING_OFF:
+                	Toast.makeText(getApplicationContext(), "bt disabled", Toast.LENGTH_SHORT).show();
+                    //onBluetoothDisable();
+                    break;
+                }
+            } 
+        }
+    };
 }
